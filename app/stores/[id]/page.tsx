a18bf86329c3bb4d6c7e2bd4c07180daac4b5c43
OhGookKwon <kwenohgook@gmail.com>
2026-01-14 19:30:30 +0900

    Fix vercel build scripts

--- app/stores/[id]/page.tsx
+++ app/stores/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
+
+export const dynamic = 'force-dynamic';
 import StaffSection from "@/app/components/StaffSection";
 import ReviewList from "@/app/components/ReviewList";
 import ReviewForm from "@/app/components/ReviewForm";
